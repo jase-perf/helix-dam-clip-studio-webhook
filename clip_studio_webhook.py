443213d557b31142147e297344959723aa7483ec
Jase Lindgren <jlindgren@perforce.com>
2024-08-07 10:51:12 -0700

    Switched to using helixdam module instead of requests

--- clip_studio_webhook.py
+++ clip_studio_webhook.py
@@ -9,17 +9,8 @@ import subprocess
 import json
 
 from flask import Flask, request, jsonify
-import requests
+from helixdam import HelixDAM, HelixDAMAuthException, HelixDAMException
 
-logging.basicConfig(
-    level=logging.DEBUG,
-    format="[%(asctime)s - %(levelname)s] %(message)s",
-    stream=sys.stdout,
-)
-
-logger = logging.getLogger(__name__)
-
-app = Flask(__name__)
 
 # Environment variables
 DAM_URL = os.environ.get("DAM_URL")
@@ -38,18 +29,29 @@ METADATA_FIELDS = {
 }
 METADATA_UUIDS = {}  # This gets populated on first run.
 
+hd = HelixDAM(url=DAM_URL, account_key=ACCOUNT_KEY)
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="[%(asctime)s - %(levelname)s] %(message)s",
+    stream=sys.stdout,
+)
+
+logger = logging.getLogger(__name__)
+
+app = Flask(__name__)
+
+
 # Queue for processing tasks
 process_queue = Queue()
 
 
 def process_file(depot_path: str) -> None:
     logger.info(f"Downloading file: {depot_path}")
-    # This is where you'd call your clip_extractor and handle the results
-
     with tempfile.NamedTemporaryFile() as temp_file:
         try:
-            download_file(depot_path, temp_file)
-        except requests.exceptions.RequestException as e:
+            hd.download_file(depot_path, temp_file)
+        except HelixDAMException as e:
             logger.error(f"Error downloading file: {e}")
             return
         temp_file.seek(0)
@@ -57,24 +59,13 @@ def process_file(depot_path: str) -> None:
 
     if file_data:
         try:
-            send_preview_to_dam(depot_path, file_data)
-        except requests.exceptions.RequestException as e:
+            hd.upload_preview(depot_path, file_data["image_data"])
+        except HelixDAMException as e:
             logger.error(f"Error sending preview: {e}")
         try:
             send_metadata_to_dam(depot_path, file_data)
-        except requests.exceptions.RequestException as e:
+        except HelixDAMException as e:
             logger.error(f"Error sending metadata: {e}")
-
-
-def download_file(depot_path: str, file_obj: tempfile.NamedTemporaryFile) -> None:
-    url = f"{DAM_URL}/api/p4/files"
-    headers = {"Authorization": f"account_key='{ACCOUNT_KEY}'"}
-    response = requests.request(
-        "GET", url, headers=headers, params={"depot_path": depot_path}
-    )
-    response.raise_for_status()
-    for chunk in response.iter_content(chunk_size=8192):
-        file_obj.write(chunk)
 
 
 def extract_clip_data(temp_file: tempfile.NamedTemporaryFile) -> dict:
@@ -99,34 +90,7 @@ def extract_clip_data(temp_file: tempfile.NamedTemporaryFile) -> dict:
         )
 
 
-def send_preview_to_dam(depot_path: str, file_data: dict) -> None:
-    logger.info(f"Uploading preview image to DAM for {depot_path}")
-    headers = {
-        "Authorization": f"account_key='{ACCOUNT_KEY}'",
-        "Content-Type": "application/json",
-    }
-    payload = {"content": file_data["image_data"], "encoding": "base64"}
-    params = {"depot_path": depot_path}
-    response = requests.put(
-        f"{DAM_URL}/api/p4/files/preview", headers=headers, params=params, json=payload
-    )
-
-    if response.status_code == 200:
-        logger.info(f"Successfully uploaded preview image to DAM for {depot_path}")
-        logger.debug(f"Response: {response.text}")
-    else:
-        logger.error(
-            f"Failed to upload preview image to DAM for {depot_path}\n{response.status_code}: {response.text}"
-        )
-
-
 def send_metadata_to_dam(depot_path: str, file_data: dict) -> None:
-    url = f"{DAM_URL}/api/p4/batch/custom_file_attributes"
-    headers = {
-        "Authorization": f"account_key='{ACCOUNT_KEY}'",
-        "Content-Type": "application/json",
-    }
-
     # Calculate our metadata values first
     metadata = {
         name: file_data["metadata"][id]
@@ -139,53 +103,18 @@ def send_metadata_to_dam(depot_path: str, file_data: dict) -> None:
             int(file_data["metadata"]["EndFrame"])
             - int(file_data["metadata"]["StartFrame"])
         )
-    logger.debug(f"Adding metadata: {metadata}")
-    payload = {
-        "paths": [{"path": depot_path}],
-        "create": [
-            {"uuid": METADATA_UUIDS[name], "value": str(value)}
-            for name, value in metadata.items()
-        ],
-        "propagatable": False,
-    }
-
-    logger.debug(f"Sending metadata to DAM: {payload}")
-    response = requests.put(
-        url,
-        headers=headers,
-        json=payload,
+    logger.debug(f"Sending metadata to DAM: {metadata}")
+    hd.update_file_metadata(
+        depot_path=depot_path,
+        uuid_value_dict={
+            METADATA_UUIDS[name]: value for name, value in metadata.items()
+        },
     )
-    response.raise_for_status()
-
     logger.info(f"Successfully uploaded metadata to DAM for {depot_path}")
 
 
 def get_or_create_metadata_fields(field_names: set) -> dict:
-    url = f"{DAM_URL}/api/company/file_attribute_templates"
-    headers = {"Authorization": f"account_key='{ACCOUNT_KEY}'"}
-    response = requests.get(url, headers=headers)
-    response.raise_for_status()
-    all_fields = response.json()["results"]
-
-    results = {f["name"]: f["uuid"] for f in all_fields if f["name"] in field_names}
-
-    fields_to_create = set(field_names).difference((f["name"] for f in all_fields))
-    for field_name in fields_to_create:
-        # If we have some to create, we can do that here:
-        payload = {
-            "name": field_name,
-            "type": "text",
-            "available_values": [],
-            "hidden": False,
-        }
-
-        response = requests.post(
-            url,
-            headers=headers,
-            json=payload,
-        )
-        response.raise_for_status()
-        results[field_name] = response.json()["uuid"]
+    results = {name: hd.create_metadata_field(name, "text") for name in field_names}
     return results
 
 
